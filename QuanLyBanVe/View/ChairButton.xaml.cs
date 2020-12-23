b2735f01f860ea8540da82bbb6eb056572f16a79
HuuNghi1999 <51283610+HuuNghi1999@users.noreply.github.com>
2020-12-23 16:47:56 +0700

    upview

---
+++ QuanLyBanVe/View/ChairButton.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyBanVe.View
+{
+    /// <summary>
+    /// Interaction logic for ChairButton.xaml
+    /// </summary>
+    public partial class ChairButton : UserControl
+    {
+        public ChairButton()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
